74550314a74c6ca61f3584f4f4ee8aa05b139105
felipevzps <felipe.vzps@gmail.com>
2024-12-24 00:10:05 -0300

    default tree view

---
+++ nvim/lua/felipevzps/netrw.lua
@@ -0,0 +1,11 @@
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 20
+
+-- always open netrw after starting nvim
+vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*",
+    command = "Vexplore"
+})
